b249ec0f2d06c22668409668cff8ad6cdbfc17a9
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2019-01-09 02:16:28 +0700

    New project

---
+++ GSuiteChromeExtension.GifMaker.Web/Startup.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GSuiteChromeExtension.Common.Infrastructures;
+using GSuiteChromeExtension.GifMaker.Web.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GSuiteChromeExtension.GifMaker.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddRazorPagesOptions(options =>
+                {
+                    options.Conventions.Add(new LanguageRouteConvention());
+                    options.Conventions.AddPageRoute("/Index", "{*url}");
+                });
+
+            var webSettings = new WebSettings();
+            this.Configuration.Bind(webSettings);
+            services.AddSingleton(webSettings);
+
+            services.AddI18n();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ GSuiteChromeExtension.GifMaker.Web/wwwroot/js/language.js
@@ -0,0 +1,57 @@
+﻿var languages;
+
+function getLanguageText(key) {
+    return $("[data-language-key='" + key + "']").html() || key;
+}
+
+function loadLanguageList() {
+    return $.ajax({
+        url: "/texts/languages.json",
+    });
+}
+
+function initLanguageTextbox() {
+    var cbo = $("[data-language-box]");
+
+    cbo.html("");
+    var selectedLanguage = $("body").attr("lang");
+
+    for (var language of languages) {
+        var item = $("<option></option>");
+        item.html(language.Name + " (" + language.EnglishName + ")");
+        item.val(language.Code);
+
+        if (language.Code == selectedLanguage) {
+            item.attr("selected", "");
+        }
+
+        cbo.append(item);
+    }
+
+    cbo.change(onLanguageSelected)
+}
+
+function onLanguageSelected() {
+    var selectedLangaugeCode = $(this).find(":selected").val();
+
+    var url = window.location.pathname || "/";
+    if (!url.startsWith("/")) {
+        url = "/" + url;
+    }
+
+    if (url.startsWith("/hl-")) {
+        var nextSplash = url.indexOf("/", 1);
+        url = url.substr(nextSplash);
+    }
+
+    url = "/hl-" + selectedLangaugeCode + url;
+    window.location.href = url;
+}
+
+$(function () {
+    loadLanguageList()
+        .done(response => {
+            languages = response;
+            initLanguageTextbox();
+        });
+});
