81b91442dc517bd8de59f36305ea2c8c8949074a
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2019-01-13 23:59:27 +0700

    tons of update

---
+++ GSuiteChromeExtension.GifMaker.Web/Controllers/FileController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GSuiteChromeExtension.GifMaker.Web.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        IHostingEnvironment env;
+        public FileController(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpGet, Route("getFile")]
+        public async Task<IActionResult> GetFile(string Token, string Id, string Name)
+        {
+            var url = $"https://www.googleapis.com/drive/v3/files/{Id}?alt=media";
+            //var item = await GetGoogleApiResponse<Stream>(url, Token);
+
+            var data = new byte[] { };
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Authorization", "Bearer " + Token);
+                data = wc.DownloadData(url);
+            }
+
+            var ms = new MemoryStream(data);
+            ms.Seek(0, SeekOrigin.Begin);
+            var item = ms.ToArray();
+
+            //try
+            //{
+            //    byte[] imageData = null;
+            //    MemoryStream ms = null;
+
+            //    try
+            //    {
+            //        using (var wc = new WebClient())
+            //        {
+            //            imageData = wc.DownloadData($"https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={item.WebViewLink}");
+            //        }
+            //        ms = new MemoryStream(imageData);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        //forbidden, proxy issues, file not found (404) etc
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    return this.StatusCode(500, e.Message);
+            //}
+
+            //var path = Path.Combine(env.WebRootPath, "Upload");
+            //Directory.CreateDirectory(path);
+
+            return File(item, "video/mp4");
+
+            //return this.Ok(new
+            //{
+            //    Path = "/Upload/" + Uri.EscapeDataString($"{request.File.Id}.docx"),
+            //});
+        }
+
+        private async Task<byte[]> GetGoogleApiResponse<T>(string url, string token)
+        {
+            Stream item = null;
+            var httpRequest = WebRequest.CreateHttp(url);
+            httpRequest.Headers.Add("Authorization", "Bearer " + token);
+
+            using (var response = (await httpRequest.GetResponseAsync()) as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new WebException($"Unexpected Status Code: {response.StatusCode} ({response.StatusDescription})");
+                }
+
+                item = response.GetResponseStream();
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                item.CopyTo(memoryStream);
+                
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+    public class ResponseItemViewModel : GoogleFile
+    {
+        [JsonProperty("webViewLink")]
+        public string WebViewLink { get; set; }
+    }
+
+    public class GenerateRequestViewModel
+    {
+        public string Token { get; set; }
+        public GoogleFile File { get; set; }
+    }
+
+    public class GoogleFile
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
--- GSuiteChromeExtension.GifMaker.Web/wwwroot/js/site.js
+++ GSuiteChromeExtension.GifMaker.Web/wwwroot/js/site.js
@@ -1,17 +1,82 @@
-﻿function getUserSelectedFile() {
-    debugger
+﻿var loopControl = $("#loop");
+var startTime = 0;
+var endTime = 0;
+
+function getUserSelectedFile() {
     var firstFile = driveFileInfo.docs[0];
 
-    $.ajax({
-        type: "GET",
-        url: "https://www.googleapis.com/drive/v3/files/" + state.ids[0] + "?alt=media",
-        beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + oauthToken); },
-        success: function (result) {
-            userMap = JSON.parse(result);
-            mapModel.setUserMap(userMap);
-        },
-        error: function (result) {
-            console.log(result);
+    var video = $("#video");
+    video.html("");
+    video[0].removeEventListener("timeupdate", videoPlayingHandler);
+
+    video.removeClass("d-none");
+
+    video[0].addEventListener('loadeddata', function () {
+        if (this.readyState >= 2) {
+            $("#controls").removeClass("d-none");
+            $("#slider-range").html("");
+
+            $("#slider-range").slider({
+                range: true,
+                min: 0,
+                max: video[0].duration,
+                values: [0, video[0].duration],
+                slide: function (e, ui) {
+                    startTime = ui.values[0];
+                    endTime = ui.values[1];
+                    $("#start-time").val(startTime);
+                    $("#end-time").val(endTime);
+
+                    var selectedDuration = ui.values[1] - ui.values[0];
+
+                    //loop listener
+                    video[0].removeEventListener("timeupdate", videoPlayingHandler);
+                    video[0].ontimeupdate = function () { videoPlayingHandler() };
+                }
+            });
+
+            $("#slider-range").removeClass("d-none");
+
+            startTime = 0;
+            endTime = video[0].duration;
+            $("#start-time").val(startTime);
+            $("#end-time").val(endTime);
         }
     });
+
+    video.append("<source src=\"" + "api/file/getFile?Token=" + oauthToken + "&Id=" + firstFile.id + "&Name=" + firstFile.name + "\" />");
+
+    //$("#amount").val("$" + $("#slider-range").slider("values", 0) +
+    //    " - $" + $("#slider-range").slider("values", 1));
 }
+
+function videoPlayingHandler() {
+    var video = $("#video")[0];
+
+    var isPlaying = !(video.paused || video.ended || video.seeking || video.readyState < video.HAVE_FUTURE_DATA);
+
+    if (video.currentTime >= endTime || video.currentTime < startTime) {
+        var scrollPrior = video.currentTime < startTime;
+
+        if (isPlaying) {
+            video.pause();
+        }
+        video.currentTime = startTime;
+        if (isPlaying) {
+            video.play();
+        }
+    }
+}
+
+$('#btn-save-to-drive').click(function () {
+    gifshot.createGIF({
+        'video': ['example.mp4']
+    }, function (obj) {
+        if (!obj.error) {
+            var image = obj.image,
+                animatedImage = document.createElement('img');
+            animatedImage.src = image;
+            document.body.appendChild(animatedImage);
+        }
+    });
+});
