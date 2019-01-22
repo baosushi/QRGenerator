5885d5a00026755148cc2f534cf5697b7c40780f
BAOTDSE62099\Temporary <baokeylog@gmail.com>
2019-01-23 02:08:09 +0700

    Init project DjVu Viewer

---
+++ GSuiteChromeExtension.DjvuViewer.Web/Controllers/FileController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GSuiteChromeExtension.DjvuViewer.Web.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        IHostingEnvironment env;
+        public FileController(IHostingEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpGet, Route("getFile")]
+        public async Task<IActionResult> GetFile(string Token, string Id)
+        {
+            var url = $"https://www.googleapis.com/drive/v3/files/{Id}?alt=media";
+            //var item = await GetGoogleApiResponse<Stream>(url, Token);
+
+            var data = new byte[] { };
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Authorization", "Bearer " + Token);
+                data = wc.DownloadData(url);
+            }
+
+            var ms = new MemoryStream(data);
+            ms.Seek(0, SeekOrigin.Begin);
+            var item = ms.ToArray();
+
+            return File(item, "application/x-shockwave-flash");
+        }
+
+        private async Task<byte[]> GetGoogleApiResponse<T>(string url, string token)
+        {
+            Stream item = null;
+            var httpRequest = WebRequest.CreateHttp(url);
+            httpRequest.Headers.Add("Authorization", "Bearer " + token);
+
+            using (var response = (await httpRequest.GetResponseAsync()) as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new WebException($"Unexpected Status Code: {response.StatusCode} ({response.StatusDescription})");
+                }
+
+                item = response.GetResponseStream();
+            }
+
+            using (var memoryStream = new MemoryStream())
+            {
+                item.CopyTo(memoryStream);
+                
+                return memoryStream.ToArray();
+            }
+        }
+    }
+
+    public class ResponseItemViewModel : GoogleFile
+    {
+        [JsonProperty("webViewLink")]
+        public string WebViewLink { get; set; }
+    }
+
+    public class GenerateRequestViewModel
+    {
+        public string Token { get; set; }
+        public GoogleFile File { get; set; }
+    }
+
+    public class GoogleFile
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ GSuiteChromeExtension.DjvuViewer.Web/wwwroot/js/site.js
@@ -0,0 +1,190 @@
+﻿var SCOPES = 'https://www.googleapis.com/auth/drive.file';
+var CLIENT_ID = $("#loader").attr("data-client-id");
+var DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
+var authLoaded = false, pickerLoaded = false;
+
+window.loginService = new LoginService(CLIENT_ID, SCOPES, DISCOVERY_DOCS);
+window.driveService = new DriveService();
+
+function onAuthApiLoad() {
+    window.loginService.initClient(updateSigninStatus);
+    authLoaded = true;
+    checkAvailable();
+    //checkDriveParams();
+}
+
+function onPickerApiLoad() {
+    pickerLoaded = true;
+    checkAvailable();
+}
+
+function onGoogleApiLoaded() {
+    gapi.load('client:auth2', onAuthApiLoad);
+    gapi.load('picker', onPickerApiLoad);
+}
+
+function checkAvailable() {
+    if (authLoaded && pickerLoaded) {
+        $("#btn-select-file").removeAttr("disabled");
+    }
+}
+
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        checkDriveParams();
+    } else {
+        loginService.signIn();
+    }
+}
+
+function checkDriveParams() {
+    var stateJson = getParameterByName("state");
+
+    if (stateJson) {
+        var state = JSON.parse(stateJson);
+        oauthToken = loginService.getAccessToken();
+
+        if (driveFileInfo) {
+            driveFileInfo = { docs: [{}] };
+        }
+
+        if (!oauthToken) {
+            loginService.signIn().then(function () {
+                oauthToken = loginService.getAccessToken();
+
+                driveFileInfo.docs[0] = {
+                    id: state.ids[0]
+                };
+
+                getUserSelectedFile();
+            });
+        } else {
+            driveFileInfo.docs[0] = {
+                id: state.ids[0]
+            };
+
+            getUserSelectedFile();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+var loopControl = $("#loop");
+var startTime = 0;
+var endTime = 0;
+
+function getUserSelectedFile() {
+    var firstFile = driveFileInfo.docs[0];
+
+    var flashContainer = $("#flash-container");
+    flashContainer.html("");
+
+    var embed = $("<embed type=\"application/x-shockwave-flash\" src=\"" + "api/file/getFile?Token=" + oauthToken + "&Id=" + firstFile.id + "\" />");
+    flashContainer.append(embed);
+
+    if (flashContainer.hasClass("d-none")) {
+        flashContainer.removeClass("d-none");
+    }
+}
+
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+//$("#btn-download").click(function () {
+//    var a = $("<a>")
+//        .attr("href", $("img#result")[0].src)
+//        .attr("download", "generated-gif.gif")
+//        .appendTo("body");
+//    a[0].click();
+//    a.remove();
+//});
+
+function createPicker() {
+    var developerKey = $("[data-developer-key]").attr("data-developer-key");
+    var appId = $("[data-app-id]").attr("data-app-id");
+
+    var docsView = new google.picker.DocsView()
+        .setIncludeFolders(false)
+        .setSelectFolderEnabled(false)
+        .setQuery("*.swf");
+
+    var picker = new google.picker.PickerBuilder()
+        .addView(docsView)
+        .enableFeature(google.picker.Feature.NAV_HIDDEN)
+        .setAppId(appId)
+        .hideTitleBar()
+        .setOAuthToken(oauthToken)
+        .setCallback(pickerCallback)
+        .build();
+
+    picker.setVisible(true);
+}
+
+function pickerCallback(info) {
+    if (info.action !== "picked" || !info.docs || !info.docs.length) {
+        return;
+    }
+
+    driveFileInfo = info;
+
+    getUserSelectedFile();
+}
+
+function onSelectFileButtonClick(callback) {
+    oauthToken = loginService.getAccessToken();
+
+    if (!oauthToken) {
+        loginService.signIn().then(function () {
+            oauthToken = loginService.getAccessToken();
+            createPicker();
+        });
+    } else {
+        createPicker();
+    }
+}
+
+$(document).ready(function () {
+    var stateJson = getParameterByName("state");
+
+    if (stateJson) {
+        var state = JSON.parse(stateJson);
+
+        onSelectFileButtonClick(function (authResult) {
+            if (authResult && !authResult.error) {
+                oauthToken = authResult.access_token;
+
+                if (driveFileInfo) {
+                    driveFileInfo = { docs: [{}] };
+                }
+
+                driveFileInfo.docs[0] = {
+                    id: state.ids[0]
+                };
+
+                getUserSelectedFile();
+            }
+        });
+    }
+});
+
+$(function () {
+    $("#btn-select-file").click(onSelectFileButtonClick);
+});
